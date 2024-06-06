ff15f1c54de0f13cfd97d8c5fa6a9ee1031c134d
LudSkop <luda.skopenko80@gmail.com>
2024-06-06 20:29:01 +0300

    added main

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
+def main():
+    print("Ласкаво просимо в асистент бота!!!>>>")
+
+    while True:
+        user_input = input("Введіть вашу команду : >>> ").strip().lower()
+        if user_input in ["exit", "close"]:
+            print("До побачення!!!")
+            break
+        elif user_input == "привіт":
+            print("Привіт чим можу допомогти?")
+        else:
+            print("Ви ввели некоректну команду!!!")
 
 
-
-
-
-
-
-
-
+if __name__ == "__main__":
+    main()
