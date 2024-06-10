629fc5fa28a6c006487fc01dca4ec1502622c7de
LudSkop <luda.skopenko80@gmail.com>
2024-06-10 19:42:44 +0300

    added locking

---
+++ locking_1.py
@@ -0,0 +1,18 @@
+def taxer(base_tax):
+    def calculate(money):
+        base_tax_1 = base_tax
+        if money >= 10_000:
+            base_tax_1 = base_tax * 2
+        return money - base_tax_1 * money
+    return calculate
+
+
+ukr = taxer(0.01)
+norv = taxer(0.04)
+
+
+money_ukr = ukr(9500)
+money_norv = norv(25000)
+
+print(money_ukr)
+print(money_norv)
