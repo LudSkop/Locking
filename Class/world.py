09604fa027f8805d92cfffdf6cfde7c9d75a6c93
LudSkop <luda.skopenko80@gmail.com>
2024-06-19 23:38:10 +0300

    added class Animal

--- Class/world.py
+++ Class/world.py
@@ -1,4 +1,34 @@
 
+class Animal:
+    def __init__(self, nickname, age):
+        self.nickname = nickname
+        self.age = age
+
+    def info_self(self):
+        return f"It's class Animal .Name is {self.nickname} and his age is {self.age}"
+
+
+class Cat(Animal):
+    name = ['cat']
+
+    def __init__(self, nickname, age, owner):
+        super().__init__(nickname, age)
+        self.owner = owner
+
+    def saund(self):
+        return f'{self.nickname} says meeey '
+
+
+cat = Cat('Alisa', 6, 'Vladyclav')
+print(cat.nickname)
+print(cat.age)
+print(cat.owner)
+print(cat.info_self())
+print(cat.saund())
+
+
+
+
 
 
 
