46a7a8c11010bd2fbf69ec520b1544a022b30ddc
LudSkop <luda.skopenko80@gmail.com>
2024-06-23 23:55:01 +0300

    create userstring

---
+++ Class/user_string.py
@@ -0,0 +1,15 @@
+#from collections  UserString
+
+
+templase = [
+    " Ви можете досягти всього. "
+    " Прості щоденні справи допоможуть досягти успіху "
+]
+
+for item, text in enumerate(templase):
+    print("|{:^5}|{:<15}".format(item,text))
+
+
+
+
+
