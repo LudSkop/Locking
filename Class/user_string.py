99257379340f6ee21893f752b1b6a2367df04ba3
LudSkop <luda.skopenko80@gmail.com>
2024-06-25 19:32:58 +0300

    create UserString

--- Class/user_string.py
+++ Class/user_string.py
@@ -1,13 +1,25 @@
-#from collections  UserString
+# Контейнер UserString
+from collections import UserString
 
 
 templase = [
-    " Ви можете досягти всього. "
-    " Прості щоденні справи допоможуть досягти успіху "
+    " Ви можете досягти всього. ",
+    " Прості щоденні справи допоможуть досягти успіху ",
+    " Боротись з прокрастинацією не складно "
 ]
 
-for item, text in enumerate(templase):
-    print("|{:^5}|{:<15}".format(item,text))
+#for item, text in enumerate(templase):
+    #print("|{:^5}|{:<15}".format(item, text))
+class Text(UserString):
+    def set_limit_text(self, limit=30):
+        return (f"{self.data[: limit -3]} ...")
+
+
+text = [Text(text) for text in templase]
+for item, text in enumerate(text):
+    print("|{:^5}|{:<15}".format(item, text.set_limit_text()))
+
+
 
 
 
