18ba29cbb7c2e187aed21a9624a5fae2fe7881ff
LudSkop <luda.skopenko80@gmail.com>
2024-06-27 19:51:51 +0300

    create magic methods

--- Class/rept.py
+++ Class/rept.py
@@ -29,6 +29,7 @@ class Point:
 a = Point(1, 9)
 print(a)
 
+
 class Human:
     def __init__(self, name, age=0):
         self.name = name
@@ -41,3 +42,45 @@ class Human:
 bill = Human('Bill')
 bill_str = str(bill)
 print(bill_str)  # Hello! I am Bill
+
+
+class ListedValuesDict:
+    def __init__(self):
+        self.data = {}
+
+    def __setitem__(self, key, value):
+        if key in self.data:
+            self.data[key].append(value)
+        else:
+            self.data[key] = [value]
+
+    def __getitem__(self, key):
+        result = str(self.data[key][0])
+        for value in self.data[key][1:]:
+            result += ", " + str(value)
+        return result
+
+
+l_dict = ListedValuesDict()
+l_dict[1] = 'a'
+l_dict[1] = 'b'
+print(l_dict[1])    # a, b
+
+
+# Функтори - це об'єкти які поводяться як функції тобто їх можна викликати та передавати
+#їм фргументи
+class Adder:
+    def __init__(self, add_value):
+        self.add_value = add_value
+
+    def __call__(self, value):
+        return self.add_value + value
+
+
+two_adder = Adder(2)
+print(two_adder(5))  # 7
+print(two_adder(4))  # 6
+
+three_adder = Adder(3)
+print(three_adder(5))  # 8
+print(three_adder(4))  # 7
