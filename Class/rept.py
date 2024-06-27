e5dcee1c6fc9e70e3811c263ffb5920220f26fa1
LudSkop <luda.skopenko80@gmail.com>
2024-06-27 14:15:26 +0300

    crete magic methods

--- Class/rept.py
+++ Class/rept.py
@@ -15,3 +15,16 @@ print(bill.age)          # 0
 jill = Human('Jill', 20)
 print(jill.say_hello())  # Hello! I am Jill
 print(jill.age)          # 20
+
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __repr__(self):
+        return f'Point ({self.x}, {self.y})'
+
+
+a = Point(1, 9)
+print(a)
