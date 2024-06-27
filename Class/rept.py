fe377e5d0d907994786d52a59ffdcb3f0904822d
LudSkop <luda.skopenko80@gmail.com>
2024-06-27 14:08:18 +0300

    create magic  methods

---
+++ Class/rept.py
@@ -0,0 +1,17 @@
+# Магічні методи
+class Human:
+    def __init__(self, name, age=0):
+        self.name = name
+        self.age = age
+
+    def say_hello(self):
+        return f'Hello! I am {self.name}'
+
+
+bill = Human('Bill')
+print(bill.say_hello())  # Hello! I am Bill
+print(bill.age)          # 0
+
+jill = Human('Jill', 20)
+print(jill.say_hello())  # Hello! I am Jill
+print(jill.age)          # 20
