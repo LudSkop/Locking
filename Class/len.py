0ec3556654eb5e2300884898bc5fbdb50b660f9f
LudSkop <luda.skopenko80@gmail.com>
2024-07-06 01:12:06 +0300

    added code

--- Class/len.py
+++ Class/len.py
@@ -1,7 +1,3 @@
-#Даний масив цілих чисел,
-# повернути суму перших двох елементів у масиві.
-# Якщо довжина масиву менше 2, просто скласти існуючі елементи,
-# повертаючи 0, якщо довжина масиву дорівнює 0.
 
 def sum2(nums):
     if len(nums) == 1:
@@ -10,3 +6,24 @@ def sum2(nums):
         return 0
     return nums[0] + nums[1]
 
+
+def caught_speeding(speed, is_birthday):
+    if is_birthday:
+        speed -= 5
+    if speed <= 60:
+        return 0
+    elif 61 <= speed <= 80:
+        return 1
+    else:
+        return 2
+
+
+
+if __name__ =='__main__':
+    print(caught_speeding(70, False))
+    print(caught_speeding(81, False))
+    print(caught_speeding(65, True))
+    print(sum2([2, 9, 5, 6]))
+    print(sum2([12, 45, 3]))
+    print(sum2([2]))
+    print(sum2([0]))
